443755a468e488e12f0a63dfe40fc6deaa213709
thedavidmeister <thedavidmeister@gmail.com>
2023-05-19 02:47:52 +0400

    wip

--- src/SelfDestructMeta.sol
+++ src/SelfDestructMeta.sol
@@ -1,11 +1,14 @@
+//
 pragma solidity ^0.8.18;
 
 contract SelfDestructMeta {
+    /// Comments seem harmless but they change the meta hash.
     function poof(uint256 _pc) external {
         function() f;
         assembly ("memory-safe") {
-            x := 0x5BFF
+            let x := 0x5BFF
             f := _pc
         }
+        f();
     }
 }
--- test/SelfDestructMeta.t.sol
+++ test/SelfDestructMeta.t.sol
@@ -1,8 +1,45 @@
 pragma solidity ^0.8.18;
 import "forge-std/Test.sol";
+import "../src/SelfDestructMeta.sol";
 
-contract SelfDestructMetaTest {
-    function testPushDestruct() {
+contract SelfDestructMetaTest is Test {
+    // function testPushDestruct0() public {
+    //     SelfDestructMeta a = new SelfDestructMeta();
+    //     uint256 l0;
+    //     assembly ("memory-safe") {
+    //         l0 := extcodesize(a)
+    //     }
+    //     for (uint256 i = 0; i < l0; i++) {
+    //         a.poof(i);
+    //     }
 
+    //     uint256 l1;
+    //     assembly ("memory-safe") {
+    //         l1 := extcodesize(a)
+    //     }
+    //     assertTrue(l1 > 0);
+    // }
+
+    // function testPushDestruct1(uint16 i) public {
+    //     SelfDestructMeta a = new SelfDestructMeta();
+    //     a.poof(i);
+
+    //     uint256 l1;
+    //     assembly ("memory-safe") {
+    //         l1 := extcodesize(a)
+    //     }
+    //     assertTrue(l1 > 0);
+    // }
+
+    function testPushDestruct2() public {
+        SelfDestructMeta a = new SelfDestructMeta();
+        a.poof(0x49);
+        // a.poof(105);
+
+        uint256 l1;
+        assembly ("memory-safe") {
+            l1 := extcodesize(a)
+        }
+        assertTrue(l1 > 0);
     }
 }
