921e5edc94ec7d64117c8af42df25137bfde77a9
thedavidmeister <thedavidmeister@gmail.com>
2023-05-19 03:31:12 +0400

    match

--- brute.sh
+++ brute.sh
@@ -1,10 +1,11 @@
 x=0
 while [ "$x" -lt 3 ]
 do
-    h=$( openssl rand 8 -hex )
+    h=$( openssl rand 16 -hex )
     echo "$x"
     echo "$h"
     echo "// $h\npragma solidity ^0.8.18;" > ./src/Comment.sol
     forge build
     x=$( cat ./out/SelfDestructMeta.sol/SelfDestructMeta.json | grep -o '5bff' | wc -l )
 done
+say "done"
--- src/Comment.sol
+++ src/Comment.sol
@@ -1,2 +1,2 @@
-// 783bc8caccc35b81
+// 4d49afc386bb08919fa6c3b3a249e4ed
 pragma solidity ^0.8.18;
