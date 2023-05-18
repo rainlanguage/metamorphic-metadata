3f24c41bfb8f9038c3223fc5cd9f7c71e44c6a91
thedavidmeister <thedavidmeister@gmail.com>
2023-05-19 02:14:51 +0400

    wip

---
+++ src/SelfDestructMeta.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.18;
+
+contract SelfDestructMeta {
+    function poof(uint256 _pc) external {
+        function() f;
+        assembly ("memory-safe") {
+            x := 0x5BFF
+            f := _pc
+        }
+    }
+}
---
+++ test/SelfDestructMeta.t.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.18;
+import "forge-std/Test.sol";
+
+contract SelfDestructMetaTest {
+    function testPushDestruct() {
+
+    }
+}
