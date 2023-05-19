9bd2ec978d5770790281f2b6e6fea04ce27bd4a5
thedavidmeister <thedavidmeister@gmail.com>
2023-05-19 13:49:09 +0400

    tests

--- test/SelfDestructMeta.t.sol
+++ test/SelfDestructMeta.t.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.8.18;
+
 import "forge-std/Test.sol";
 import "../src/SelfDestructMeta.sol";
 
@@ -35,6 +36,40 @@ contract SelfDestructMetaTest is Test {
         SelfDestructMeta a = new SelfDestructMeta();
         a.poof(0x49);
         // a.poof(105);
+
+        uint256 l1;
+        assembly ("memory-safe") {
+            l1 := extcodesize(a)
+        }
+        assertTrue(l1 > 0);
+    }
+
+    function testPushDestruct3() public {
+        SelfDestructMeta a = new SelfDestructMeta();
+        console.logBytes(address(a).code);
+
+        // a.poof(258);
+
+        for (uint256 i = 0; i < 600; i++) {
+            if (
+                i == 63 || i == 67 || i == 69 || i == 99 || i == 106 || i == 116 || i == 133 || i == 149 || i == 173
+                    || i == 174
+            ) {
+                continue;
+            }
+
+            vm.expectRevert();
+            a.poof(i);
+        }
+        a.poof(63);
+        a.poof(67);
+        a.poof(69);
+        a.poof(106);
+        a.poof(116);
+        a.poof(133);
+        a.poof(149);
+        a.poof(173);
+        a.poof(174);
 
         uint256 l1;
         assembly ("memory-safe") {
